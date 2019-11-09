77edd37468c978ca888722a743211ea027ec48f7
Frank Martin <cedricmrtn@gmail.com>
2019-11-08 23:05:35 -0700

    Scanner imported. Added pressButton and turnOn method.

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		
+		Scanner kb = new Scanner(System.in);
 
 	}
-	
-	// TODO: Add pressButton() method
-	// TODO: Add turnOn() method
 
+	// TODO: Add pressButton() method
+	public static void pressButtion() {
+		System.out.println("");
+	}
+
+	// TODO: Add turnOn() method
+	public static void turnOn() {
+		System.out.println("");
+	}
 }
