acba688d7473d4956492ce45293d73e36029f0bb
Frank Martin <cedricmrtn@gmail.com>
2019-11-09 14:03:52 -0700

    turnOn method now implimented

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -4,7 +4,10 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-
+		
+		System.out.println("To begin, turn on the remote control by entering 'ON': ");
+		kb.next();
+		turnOn();
 	}
 
 	// TODO: Add pressButton() method
@@ -14,6 +17,6 @@ public class RemoteControlApp {
 
 	// TODO: Add turnOn() method
 	public static void turnOn() {
-		System.out.println("");
+		System.out.println("TURNING ON...");
 	}
 }
