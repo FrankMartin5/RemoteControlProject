caa20b2450bab413cdbca99e2004108b581c5577
Frank Martin <cedricmrtn@gmail.com>
2019-11-09 15:40:38 -0700

    Created a remote visual. Added a do while loop with a switch statement for button entry. Added turn off method if user is done with the remote. Added a while loop and if else statement to turn on the remote

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -4,19 +4,69 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		
+		String input = "";
+		String on;
+		printRemote();
 		System.out.println("To begin, turn on the remote control by entering 'ON': ");
-		kb.next();
-		turnOn();
-	}
+		while(true) {
+			on = kb.next();
+			if (on.equalsIgnoreCase("On")) {
+				turnOn();
+				break;
+			} else {
+				System.out.println("Wrong input. Try again.");
+			}
+			
+		}
+		
+		System.out.println("Enter a button number: ");
+		
+		do {
+			input = kb.next();
+			switch(input) {
+			case "0":
+			case "1":
+			case "2":	
+			case "3":	
+			case "4":	
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+				pressButtion();
+				break;
+			case "Off":
+			}
+		} while (!input.equalsIgnoreCase("Off"));
+		turnOff();
 
+		
+		
+		
+		kb.close();
+		
+	}
+	public static void printRemote() {
+		System.out.println("***********");
+		System.out.println("* ON  OFF *");
+		System.out.println("* 1  2  3 *");
+		System.out.println("* 4  5  6 *");
+		System.out.println("* 7  8  9 *");
+		System.out.println("*    0    *");
+		System.out.println("***********");
+	}
 	// TODO: Add pressButton() method
 	public static void pressButtion() {
-		System.out.println("");
+		System.out.println("BOOP!");
 	}
 
 	// TODO: Add turnOn() method
 	public static void turnOn() {
 		System.out.println("TURNING ON...");
 	}
+	public static void turnOff() {
+		System.out.println("TURNING Off...");
+	}
+
 }
